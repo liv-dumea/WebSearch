c49fb100a00acac16d6352686667796875970746
livil <liv.dumea@yahoo.ro>
2012-12-16 14:47:35 +0200

    the same as previous

---
+++ WebSearch/ScreenResults.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WebSearch
+{
+    partial class ScreenResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.desciptionLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsTextBox.Location = new System.Drawing.Point(13, 34);
+            this.resultsTextBox.MaxLength = 100000;
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ReadOnly = true;
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.resultsTextBox.Size = new System.Drawing.Size(845, 421);
+            this.resultsTextBox.TabIndex = 2;
+            // 
+            // desciptionLabel
+            // 
+            this.desciptionLabel.AutoSize = true;
+            this.desciptionLabel.Location = new System.Drawing.Point(21, 9);
+            this.desciptionLabel.Name = "desciptionLabel";
+            this.desciptionLabel.Size = new System.Drawing.Size(16, 13);
+            this.desciptionLabel.TabIndex = 1;
+            this.desciptionLabel.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.CausesValidation = false;
+            this.button1.Location = new System.Drawing.Point(12, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ScreenResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.desciptionLabel);
+            this.Controls.Add(this.resultsTextBox);
+            this.Name = "ScreenResults";
+            this.Text = "ScreenResults";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.Label desciptionLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
